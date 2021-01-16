f8cfcae9000a869b1ba8be8fe1ce975157ce3119
Hayley Patton <theemacsshibe@gmail.com>
2021-01-16 14:23:16 +1100

    Compute the binomial table with ratios and not floating point

--- Snippets/generate-cdf-table.lisp
+++ Snippets/generate-cdf-table.lisp
@@ -9,9 +9,9 @@
   (loop for k from minimum to n
         sum (binomial-pmf k n p)))
 
-(defun binomial-table (n p)
+(defun binomial-table (n p &key (maximum n))
   (loop with one = (binomial-cdf 0 n p)
-        for k from 0 to n
+        for k from 0 to maximum
         for x = (min (floor (* (expt 2 64)
                                (/ (binomial-cdf k n p) one)))
                      (1- (expt 2 64)))
@@ -21,10 +21,10 @@
 (defun print-binomial-table (n p
                              &key (variable-name "binomial_table")
                                   (length-name "BINOMIAL_TABLE_LENGTH"))
-  (let ((table (append (binomial-table n p) (list 0))))
+  (let ((table (append (binomial-table n p :maximum 64) (list 0))))
     (format t "~&__constant unsigned long ~a[] = {~%~{  0x~16,'0x~^, 0x~16,'0x~^, 0x~16,'0x~^, 0x~16,'0x~^,~%~}~%};"
             variable-name
             table)
     (format t "~&#define ~a ~d" length-name (length table))))
 
-;; (print-binomial-table 262 (float (/ 20 423) 1.0d0))
+(print-binomial-table 262 20/423)
